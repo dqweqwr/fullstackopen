9268b34f252a2ea8061abb98f34b04b0a60f6f3a
dotaplayer1 <georgemao72@gmail.com>
2023-06-24 15:47:48 -0700

    Turn App constants into single object

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,45 +1,59 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.name} {props.exercises}</p>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {props.course.parts.map(c => {
+        return (<Part name={c.name} exercises={c.exercises} />)
+      })}
     </div>
   )
 }
 
 const Total = (props) => {
+  const num_exercises = props.course.parts.reduce((result, item) => {
+    return result + item.exercises;
+  }, 0)
+
   return (
-    <p>Number of exercises {props.num_exercises}</p>
+    <p>Number of exercises: {num_exercises}</p>
   )
 }
 
 function App() {
-  const course = "Half Stack Application Development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const course = {
+    name: "Half Stack Application Development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      },
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total num_exercises={exercises1 + exercises2 + exercises3} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 }
