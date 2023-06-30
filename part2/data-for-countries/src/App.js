890cfb5312cef3b516b1332e5cc204136b99dde6
dotaplayer1 <georgemao72@gmail.com>
2023-06-29 18:08:19 -0700

    Add country querying functionality

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
 
-function App() {
+const SearchBar = ({ filter, handleFilterChange }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="searchbar">
+        Find countries: 
+      </label>
+      <input
+        id="searchbar"
+        type="text"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+    </div>
+  )
+}
+
+const CountryView = ({ countries, filter }) => {
+  if (filter !== "") {
+    const filterRegex = new RegExp(filter, "i")
+    countries = countries.filter(country => {
+      return country.match(filterRegex)
+    })
+  }
+
+  if (filter === "") {
+    return <div></div>
+  } else if (countries.length > 10) {
+    return <div>Too many matches. Please specify another filter</div>
+  } else if (countries.length === 1) {
+    return <div>One match: {countries[0]}</div>
+  } else if (countries.length === 0) {
+    return <div>No matches found</div>
+  }
+
+  return (
+    <div>
+      {countries.sort().map((country, index) => {
+        return <div key={index}>{country}</div>
+      })}
+    </div>
+  )
+}
+
+const App = () => {
+  const [filter, setFilter] = useState("")
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then(response => response.data)
+      .then(responseData => {
+        const countryNames = responseData.map(country => {
+          return country.name.common
+        })
+        setCountries(countryNames)
+        console.log(countryNames)
+      })
+  }, [])
+
+  if (!countries) return null
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <SearchBar
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+      <CountryView countries={countries} filter={filter} />
     </div>
   );
 }
