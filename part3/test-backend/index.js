4cffc269ef6bcf49c1e933038c5bf2a82f62ca1c
dotaplayer1 <georgemao72@gmail.com>
2023-07-03 15:05:29 -0700

    Get backend to write to MongoDB database

--- part3/test-backend/index.js
+++ part3/test-backend/index.js
@@ -50,13 +50,9 @@ app.get("/api/notes", (request, response) => {
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -66,30 +62,21 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post("/api/notes", (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing"
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
