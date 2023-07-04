c55deb93ed4def3f816d64e7f16318ae85c5e5fb
dotaplayer1 <georgemao72@gmail.com>
2023-07-04 13:58:10 -0700

    Reorganized file structure

---
+++ part4/notes-backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log("give password as argument")
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://pnpmtrud:${password}@cluster0.f8nm1u5.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model("Note", noteSchema)
+
+// const note = new Note({
+//   content: "mongo",
+//   important: false,
+// })
+
+// note.save().then(result => {
+//   console.log(result)
+//   console.log("Note saved!")
+//   mongoose.connection.close()
+// })
+
+Note.find({ important: false }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
