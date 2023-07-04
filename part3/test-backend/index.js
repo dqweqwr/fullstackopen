451c2aeaada09bc32cf8d216224ffb5586563ad5
dotaplayer1 <georgemao72@gmail.com>
2023-07-04 11:59:40 -0700

    Add input validation

--- part3/test-backend/index.js
+++ part3/test-backend/index.js
@@ -45,7 +45,7 @@ app.get("/", (request, response) => {
 })
 
 // create
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -57,9 +57,11 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 // read
@@ -83,14 +85,18 @@ app.get("/api/notes/:id", (request, response, next) => {
 
 // update
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
   const note = {
     content: body.content,
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -116,6 +122,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
