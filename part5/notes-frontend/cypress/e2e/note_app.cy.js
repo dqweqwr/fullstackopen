5dd89ea32b602a8ef9adb628f99d34b3b1328aa5
dotaplayer1 <georgemao72@gmail.com>
2023-07-13 16:27:42 -0700

    More cypress tests

--- part5/notes-frontend/cypress/e2e/note_app.cy.js
+++ part5/notes-frontend/cypress/e2e/note_app.cy.js
@@ -4,11 +4,12 @@ describe("Note app", function() {
   const password = "testpassword"
 
   beforeEach(function() {
-    cy.request("POST", "http://localhost:3000/api/testing/reset")
+    cy.request("POST", `${Cypress.env("BACKEND")}/testing/reset`)
     const user = { name, username, password }
-    cy.request("POST", "http://localhost:3000/api/users", user)
-    cy.visit("localhost:3000")
+    cy.request("POST", `${Cypress.env("BACKEND")}/users`, user)
+    cy.visit("")
   })
+
   it("front page can be opened", function() {
     cy.contains("Notes")
     cy.contains("Note app 2023")
@@ -23,12 +24,22 @@ describe("Note app", function() {
     cy.contains(`${name} logged in`)
   })
 
+  it("login fails with wrong password", function() {
+    cy.contains("login").click()
+    cy.get("#username").type(username)
+    cy.get("#password").type("wrongpassword")
+    cy.get("button[type='submit']").click()
+
+    cy.get(".error")
+      .should("contain" ,"Wrong credentials")
+      .and("have.css", "color", "rgb(255, 0, 0)")
+
+    cy.get("html").should("not.contain", `${name} logged in`)
+  })
+
   describe("when logged in", function() {
     beforeEach(function() {
-      cy.contains("login").click()
-      cy.get("#username").type(username)
-      cy.get("#password").type(password)
-      cy.get("button[type='submit']").click()
+      cy.login({ username, password })
     })
 
     it("a new note can be created", function() {
@@ -39,24 +50,40 @@ describe("Note app", function() {
       cy.contains(input)
     })
 
-    describe("and a note exists", () => {
+    describe("and a note exists", function() {
       const input = "hello world asdf"
 
       beforeEach(function() {
-        cy.get(".toggle").click()
-        cy.get("input[placeholder='write note content here']").type(input)
-        cy.contains("save").click()
+        cy.createNote({
+          content: input,
+          important: true
+        })
       })
 
       it("it can be made not important", function() {
-        cy.contains(input)
-          .contains("make not important")
+        cy.contains("make not important")
           .click()
 
-        cy.contains(input)
-          .contains("make important")
+        cy.contains("make important")
       })
     })
 
+    describe("and several notes exist", function() {
+      beforeEach(function () {
+        cy.createNote({ content: "first note", important: false })
+        cy.createNote({ content: "second note", important: false })
+        cy.createNote({ content: "third note", important: false })
+      })
+
+      it("one of those can be made important", function() {
+        cy.contains("second note")
+          .parent()
+          .find("button")
+          .as("theButton")
+
+        cy.get("@theButton").click()
+        cy.get("@theButton").should("contain", "make not important")
+      })
+    })
   })
 })
