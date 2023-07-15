1dcff36f78227465cf9afe28a3bf8015b66191c4
dotaplayer1 <georgemao72@gmail.com>
2023-07-15 12:12:50 -0700

    More redux

--- part6/redux-test/src/index.js
+++ part6/redux-test/src/index.js
@@ -4,42 +4,48 @@ import './index.css';
 
 import { createStore } from 'redux';
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1
-    case "DECREMENT":
-      return state - 1
-    case "ZERO":
-      return 0
-    default:
-      return state
+const noteReducer = (state = [], action) => {
+  if (action.type === "NEW_NOTE") {
+    return state.concat(action.payload)
   }
+
+  return state
 }
 
-const store = createStore(counterReducer)
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the state is in redux store",
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2
+  }
+})
 
 const App = () => {
   return (
     <>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: "INCREMENT" })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: "DECREMENT" })}
-      >
-        minus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: "ZERO" })}
-      >
-        zero
-      </button>
+      <ul>
+        {store.getState().map(note => {
+          return (
+            <li key={note.id}>
+              {note.content}
+              {" "}
+              <strong>{note.important ? "important" : "not important"}</strong>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
