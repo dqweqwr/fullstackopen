68b45b2f4de29a544fafe196323f184b995b4776
dotaplayer1 <georgemao72@gmail.com>
2023-07-15 13:50:10 -0700

    Split code into different files + use provider component to access state

--- part6/redux-test/src/App.js
+++ part6/redux-test/src/App.js
@@ -1,7 +1,44 @@
-function App() {
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
+
+const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => {
+    console.log(state)
+    return state
+  })
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ""
+    dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
   return (
     <>
-      Hello world
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map(note => {
+          return (
+            <li
+              key={note.id}
+              onClick={() => toggleImportance(note.id)}
+            >
+              {note.content}
+              {" "}
+              <strong>{note.important ? "important" : "not important"}</strong>
+            </li>
+          )
+        })}
+      </ul>
     </>
   );
 }
--- part6/redux-test/src/index.js
+++ part6/redux-test/src/index.js
@@ -1,62 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.payload)
-  }
-
-  return state
-}
+import App from "./App"
+import noteReducer from "./reducers/noteReducer"
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the state is in redux store",
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return (
-    <>
-      <ul>
-        {store.getState().map(note => {
-          return (
-            <li key={note.id}>
-              {note.content}
-              {" "}
-              <strong>{note.important ? "important" : "not important"}</strong>
-            </li>
-          )
-        })}
-      </ul>
-    </>
-  )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(() => {
-  console.log("store changed")
-  renderApp()
-})
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
