ee629eba3a7aff1fad9f5988e62601d271d98cbc
dotaplayer1 <georgemao72@gmail.com>
2023-07-14 18:53:04 -0700

    Write counter app using redux

--- part6/redux-test/src/index.js
+++ part6/redux-test/src/index.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import { createStore } from 'redux';
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1
+    case "DECREMENT":
+      return state - 1
+    case "ZERO":
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <>
+      <div>
+        {store.getState()}
+      </div>
+      <button
+        onClick={e => store.dispatch({ type: "INCREMENT" })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: "DECREMENT" })}
+      >
+        minus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: "ZERO" })}
+      >
+        zero
+      </button>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(() => {
+  console.log("store changed")
+  renderApp()
+})
