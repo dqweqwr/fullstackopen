2c14b7cb890f66cc2db137943b9cb617837357e2
dotaplayer1 <georgemao72@gmail.com>
2023-07-18 15:30:03 -0700

    Create counter app with useReducer

---
+++ part6/usereducer-test/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import { CounterContextProvider } from './CounterContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+);
+
