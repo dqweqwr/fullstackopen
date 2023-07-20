441ff5d5e5bef790432419cbee4d6402d6a860c9
dotaplayer1 <georgemao72@gmail.com>
2023-07-19 17:48:40 -0700

    Add useField hook to AnecdotesForm

--- part7/routed-anecdotes/src/components/AnecdotesForm.js
+++ part7/routed-anecdotes/src/components/AnecdotesForm.js
@@ -1,22 +1,27 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks/index"
 
 const AnecdotesForm = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
   const navigate = useNavigate()
+  const { reset: resetContent, ...content } = useField("content")
+  const { reset: resetAuthor, ...author } = useField("author")
+  const { reset: resetInfo, ...info } = useField("info")
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate("/")
+  }
+
+  const resetForm = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -25,17 +30,20 @@ const AnecdotesForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={resetForm}>
+          reset
+        </button>
       </form>
     </div>
   )
