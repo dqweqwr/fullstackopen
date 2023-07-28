9333ff1d9b6faacbc8742c77a56cc107004c42e0
dotaplayer1 <georgemao72@gmail.com>
2023-07-28 12:40:44 -0700

    Add login stuff

--- part8/graphql-test-frontend/src/App.js
+++ part8/graphql-test-frontend/src/App.js
@@ -1,15 +1,17 @@
-import { useQuery } from "@apollo/client"
+import { useApolloClient, useQuery } from "@apollo/client"
 import { useState } from "react"
 import PersonForm from "./components/PersonForm"
 import PhoneForm from "./components/PhoneForm"
 import Persons from "./components/Persons"
 import { ALL_PERSONS } from "./queries"
 import Notification from "./components/Notification"
+import LoginForm from "./components/LoginForm"
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
-  console.log(result.data)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>Loading...</div>
@@ -22,9 +24,26 @@ const App = () => {
     }, 4000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.removeItem("phonenumbers-user-token")
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <>
+        <Notification errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
     <>
       <Notification errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
       <Persons persons={result.data.allPersons} />
--- part8/graphql-test-frontend/src/components/PersonForm.js
+++ part8/graphql-test-frontend/src/components/PersonForm.js
@@ -19,7 +19,14 @@ const PersonForm = ({ setError }) => {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({
+      variables: {
+        name,
+        street,
+        city,
+        phone: phone.length > 0 ? phone : undefined,
+      },
+    })
 
     setName("")
     setPhone("")
