bc8dafd59c2e88848cf7e50800c89640715f50fd
dotaplayer1 <georgemao72@gmail.com>
2023-08-05 17:19:03 -0700

    Add database container

--- part12/todo-app/todo-backend/routes/todos.js
+++ part12/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const updatedTodo = req.body
+  await req.todo.update(updatedTodo)
+  res.send(req.todo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
